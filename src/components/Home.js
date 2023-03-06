14fde8319df19afd83db43f67e860d65f73102d0
Hayden Nault <naulth@gmail.com>
2023-03-06 14:44:26 -0500

    working on home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Home() {
     return(
-        <h1>Hello World</h1>
+        <div>
+            <div>
+                <h1>The Science-Fiction and Fantasy Database</h1>
+            </div>
+            <div>
+                <p>The Science-Fiction and Fantasy Database is an ever growing collection of your favorite movies and books, along with some titles you may have yet to discover.</p>
+                <p>Browse our catalogs</p>
+            </div>
+        </div>
     )
 }
 
