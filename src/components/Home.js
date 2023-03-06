598947e63b42e5a27cc834fd15bb95d37c4e1fce
Hayden Nault <naulth@gmail.com>
2023-03-06 14:55:48 -0500

    home componenet basics

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,10 @@ function Home() {
             </div>
             <div>
                 <p>The Science-Fiction and Fantasy Database is an ever growing collection of your favorite movies and books, along with some titles you may have yet to discover.</p>
-                <p>Browse our catalogs</p>
+                <p>Browse our catalogs to find your favorite titles, and if you don't see a personal favorite, please participate in this collaboritive project by submitting an entry.</p>
+            </div>
+            <div>
+                <p>Authors: Hayden Nault & Jesse Hunter & Katelynn Morris</p>
             </div>
         </div>
     )
