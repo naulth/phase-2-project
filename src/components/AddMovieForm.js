10031e7d7c26de7315238bf9db562992190519b8
Hayden Nault <naulth@gmail.com>
2023-03-07 10:20:24 -0500

    new form and component changes

--- src/components/AddMovieForm.js
+++ src/components/AddMovieForm.js
@@ -1,26 +1,26 @@
 import React, {useState, useEffect} from "react"
 
-function AddBookForm() {
+function AddMovieForm() {
 
     const [title, setTitle] = useState('')
     const [thumbnail, setThumbnail] = useState('')
-    const [author, setAuthor] = useState('')
+    const [director, setDirector] = useState('')
     const [summary, setSummary] = useState('')
     const [ref, setRef] = useState('')
 
     const handleTitle = (e) => {setTitle(e.target.value)}
     const handleThumbnail = (e) => {setThumbnail(e.target.value)}
-    const handleAuthor = (e) => {setAuthor(e.target.value)}
+    const handleDirector = (e) => {setDirector(e.target.value)}
     const handleSummary = (e) => {setSummary(e.target.value)}
     const handleRef = (e) => {setRef(e.target.value)}
 
     const handleSubmit = (e) => {
         e.preventDefault()
     
-        const newBook ={
+        const newMovie ={
             title: title,
             thumbnail: thumbnail,
-            author: author,
+            author: director,
             summary: summary,
             ref: ref
         }
@@ -31,15 +31,15 @@ function AddBookForm() {
         <div>
           <input onChange={handleTitle} type="text" name="title" placeholder="Title"/>
           <input onChange={handleThumbnail} type="text" name="thumbnail" placeholder="Thumbnail URL" />
-          <input onChange={handleAuthor} type="text" name="author" placeholder="Author" />
+          <input onChange={handleDirector} type="text" name="director" placeholder="Director" />
           <input onChange={handleSummary} type="text" name="summary" placeholder="Summary"/>
           <input onChange={handleRef} type="text" name="ref" placeholder="Referance URL"/>
         </div>
         <button type="submit">
-          Add A Book
+          Add A Movie
         </button>
       </form>
     )
 }
-export default AddBookForm
+export default AddMovieForm
 
