b50d396a8ce2d8fa664317da17512df417da297d
Hayden Nault <naulth@gmail.com>
2023-03-07 15:39:13 -0500

    typo changes

--- src/components/AddBookForm.js
+++ src/components/AddBookForm.js
@@ -2,13 +2,13 @@ import React, {useState} from "react"
 
 function AddBookForm({addBook}) {
 
-    const [title, setTitle] = useState('')
+    const [name, setName] = useState('')
     const [thumbnail, setThumbnail] = useState('')
     const [author, setAuthor] = useState('')
     const [summary, setSummary] = useState('')
     const [ref, setRef] = useState('')
 
-    const handleTitle = (e) => {setTitle(e.target.value)}
+    const handleName = (e) => {setName(e.target.value)}
     const handleThumbnail = (e) => {setThumbnail(e.target.value)}
     const handleAuthor = (e) => {setAuthor(e.target.value)}
     const handleSummary = (e) => {setSummary(e.target.value)}
@@ -18,7 +18,7 @@ function AddBookForm({addBook}) {
         e.preventDefault()
     
         const newBook ={
-            title: title,
+            name: name,
             thumbnail: thumbnail,
             author: author,
             summary: summary,
@@ -37,7 +37,7 @@ function AddBookForm({addBook}) {
     return(
       <form onSubmit={handleSubmit}>
         <div>
-          <input onChange={handleTitle} type="text" name="title" placeholder="Title"/>
+          <input onChange={handleName} type="text" name="name" placeholder="Name"/>
           <input onChange={handleThumbnail} type="text" name="thumbnail" placeholder="Thumbnail URL" />
           <input onChange={handleAuthor} type="text" name="author" placeholder="Author" />
           <input onChange={handleSummary} type="text" name="summary" placeholder="Summary"/>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,7 @@ import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import {NavLink} from 'react-router-dom'
+import '../index.css';
 
 function TheNavBar() {
 
