2cece7d5bbf77934e28d0acb78a9592da122d399
Hayden Nault <naulth@gmail.com>
2023-03-07 19:23:46 -0500

    hide the forms

--- src/components/BooksPage.js
+++ src/components/BooksPage.js
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import BookCards from './BookCard'
 import CardGroup from 'react-bootstrap/CardGroup'
 import AddBookForm from './AddBookForm'
+import '../index.css';
 
 
 function BooksPage({booksArray, addBook}){
     
     const booksComponents = booksArray.map(book => <BookCards key={book.id} bookName={book.name} bookImg={book.thumbnail}/>)
 
+    const [hideBookForm, setHideBookForm] = useState(true)
+    const handleHideBookForm = () => {
+        setHideBookForm(hideBookForm => !hideBookForm)
+    }
+
+    function FormButton({handleHideBookForm}) {
+        return(
+            <button onClick={handleHideBookForm} className="hideFormButton">Add a Book</button>
+        )
+    }
+
     return(
         <main>
             <div className="bookdiv">
                 <h2>Science-Fiction and Fantasy Books</h2>
-                <AddBookForm addBook={addBook}/>
+                <div className="formdiv">
+                    {hideBookForm ? <FormButton handleHideBookForm={handleHideBookForm} className="hideFormButton" /> : <AddBookForm addBook={addBook} handleHideBookForm={handleHideBookForm}/>}
+                </div>
+            
             </div>
             <div>
                 <CardGroup>
--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import MovieCards from './MovieCard'
 import CardGroup from 'react-bootstrap/CardGroup'
 import AddMovieForm from './AddMovieForm.js'
@@ -9,11 +9,26 @@ function MoviesPage({moviesArray, addMovie}){
     
     const moviesComponents = moviesArray.map(movie => <MovieCards key={movie.id} movieName={movie.name} movieImg={movie.thumbnail}/>)
 
+
+    const [hideMovieForm, setHideMovieForm] = useState(true)
+    const handleHideMovieForm = () => {
+        setHideMovieForm(hideMovieForm => !hideMovieForm)
+    }
+
+    function FormButton({handleHideMovieForm}) {
+        return(
+            <button onClick={handleHideMovieForm} className="hideFormButton">Add a Movie</button>
+        )
+    }
+
+
     return(
         <main>
             <div className="moviediv">
                 <h2>Science-Fiction and Fantasy Movies</h2>
-                <AddMovieForm addMovie={addMovie}/>
+                <div className="formdiv">
+                    {hideMovieForm ? <FormButton handleHideMovieForm={handleHideMovieForm} className="hideFormButton" /> : <AddMovieForm addMovie={addMovie} handleHideMovieForm={handleHideMovieForm}/>}
+                </div>
             </div>
             <div>
                 <CardGroup>
